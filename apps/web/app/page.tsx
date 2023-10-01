17cabe152a4fd08940f3a44892612285b821c6e2
anas-araid <araid.anas99@gmail.com>
2023-10-01 23:44:17 +0200

    chore: style dictionary

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         className='absolute left-0 top-0 h-full w-full object-cover overflow-hidden scale-150 transform-gpu'
         alt={'bg'}
       />
-      <div className="flex flex-col gap-8 pb-8 md:gap-16 md:pb-16 xl:pb-24">
+      <div className="relative flex flex-col gap-8 pb-8 md:gap-16 md:pb-16 xl:pb-24">
         <div className='flex flex-col items-center justify-center max-w-3xl px-8 mx-auto mt-8 sm:min-h-screen sm:mt-0 sm:px-0'>
           <h1 className="py-4 text-5xl font-bold tracking-tight text-center text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white sm:text-7xl">
             Tokenwind
@@ -18,10 +18,10 @@ export default function Home() {
             Instantly convert your Figma&apos;s design token into ready to import Tailwind themes.
           </p>
           <div className="flex flex-col justify-center gap-4 mx-auto mt-8 sm:flex-row sm:max-w-lg ">
-            <a className="w-64 text-center inline-block space-x-2 rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-white/80 hover:bg-white/10 hover:text-white duration-150 hover:drop-shadow-cta ring-0" href="/deploy">
+            <a className="w-32 text-center inline-block space-x-2 rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-white/80 hover:bg-white/10 hover:text-white duration-150 hover:drop-shadow-cta ring-0" href="/deploy">
               Docs
             </a>
-            <a className="w-64 text-center transition-all space-x-2 rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800 bg-zinc-50 hover:bg-zinc-50/80 ring-1 ring-transparent hover:ring-zinc-600/80 duration-150 hover:drop-shadow-cta" href="/share">
+            <a className="w-32 text-center transition-all space-x-2 rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800 bg-zinc-50 hover:bg-zinc-50/80 ring-1 ring-transparent hover:ring-zinc-600/80 duration-150 hover:drop-shadow-cta" href="/share">
               <span>Convert</span>
             </a>
           </div>
