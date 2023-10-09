bafa9adafa9dc3925468cbd7b986dcd49d4fa882
anas-araid <araid.anas99@gmail.com>
2023-10-10 00:08:21 +0200

    feat: added badge

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,35 @@
 'use client';
 /* eslint-disable @next/next/no-img-element */
 import { Snippet } from "@nextui-org/snippet";
-import { Button } from "@/components/ui/button"
-import { Copy } from "lucide-react"
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Copy, Check } from "lucide-react";
 import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/components/ui/use-toast";
+import { Badge } from "@/components/ui/badge";
 
 const COMMAND = 'npx tokenwind@latest generate';
 
 export default function Home() {
   const [_, copy] = useCopyToClipboard()
   const { toast } = useToast();
+  const [isCheckVisible, setCheckVisible] = useState(false);
 
   const handleCopy = async () => {
     const res = await copy(COMMAND);
 
     if (res) {
+      setCheckVisible(true)
+
+      setTimeout(() => {
+        setCheckVisible(false)
+      }, 5000);
+
       toast({
         variant: 'default',
         description: "Command has been copied to clipboard!",
-        className: 'text-white'
+        className: 'text-white',
+        action: undefined
       })
     }
   }
@@ -33,10 +43,11 @@ export default function Home() {
       />
       <div className="relative flex flex-col gap-8 pb-8 md:gap-16 md:pb-16 xl:pb-24">
         <div className='flex flex-col items-center justify-center max-w-3xl px-8 mx-auto mt-8 sm:min-h-screen sm:mt-0 sm:px-0'>
+          <Badge variant="outline" className="z-10 text-zinc-300 leading-5 border-zinc-600">Currently used for experimental purposes</Badge>
           <h1 className="py-4 text-5xl font-bold tracking-tight text-center text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white sm:text-7xl">
             Tokenwind
-            {/* Convert design tokens<br></br>into <br></br>Tailwind themes */}
           </h1>
+
           <p className="mt-6 leading-5 text-zinc-300 text-center">
             Instantly convert your W3C design tokens into ready to import Tailwind themes.
           </p>
@@ -58,10 +69,10 @@ export default function Home() {
               <Button
                 variant="ghost"
                 size="icon"
-                className="absolute z-10 right-3 h-8 w-8 top-1/2 -translate-y-1/2 hover:bg-white hover:bg-opacity-10 hover:text-white"
+                className="absolute z-10 right-3 h-8 w-8 top-1/2 -translate-y-1/2 text-white hover:bg-white hover:bg-opacity-10 hover:text-white"
                 onClick={handleCopy}
               >
-                <Copy className="h-4 w-4" />
+                {isCheckVisible ? <Check className="h-4 w-4 fade-in-25 animate-in" /> : <Copy className="h-4 w-4 fade-in-25 animate-in" />}
               </Button>
             </div>
           </div>
